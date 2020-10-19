6dbbc9e53306141e19ae522dbf992162d89663cf
aditya Kharade <adityakharade51@gmail.com>
2020-10-19 21:59:00 +0530

    Refactored : refactored code in AddressBookService

--- AddressBookService.java
+++ AddressBookService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class AddressBookService {
-	public static String PAYROLL_FILE_NAME = "AddressBook.txt";
+	public static String FILE_NAME = "AddressBook.txt";
 
 	public void writeData(Map<String, AddressBook> addressBookMap) {
 		StringBuffer employeeBuffer = new StringBuffer();
@@ -17,14 +17,14 @@ public class AddressBookService {
 		});
 		}
 		try {
-			Files.write(Paths.get(PAYROLL_FILE_NAME), employeeBuffer.toString().getBytes());
+			Files.write(Paths.get(FILE_NAME), employeeBuffer.toString().getBytes());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	public void readData() {
 		try {
-			Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
+			Files.lines(new File(FILE_NAME).toPath()).forEach(System.out::println);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
