3aee64cee43423135f936ac6bde633e288102dd1
aditya Kharade <adityakharade51@gmail.com>
2020-10-21 17:29:35 +0530

    Added: added functionality to perfom JSON file operations

--- AddressBookService.java
+++ AddressBookService.java
@@ -12,12 +12,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonStreamParser;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 public class AddressBookService {
 	public static String FILE_NAME = "AddressBook.txt";
 	public static String CSV_FILE = "AddressBook.csv";
+	public static String JSON_FILE = "AddressBook.json";
 
 	public void writeData(Map<String, AddressBook> cityBookMap) {
 		StringBuffer employeeBuffer = new StringBuffer();
@@ -80,6 +84,49 @@ public class AddressBookService {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Usecase15 using GSON writing data to JSON file
+	 * 
+	 * @param cityBookMap
+	 * @throws IOException
+	 */
+	public void writeDataToJSON(Map<String, AddressBook> cityBookMap) throws IOException {
+		Gson gson = new Gson();
+		Path path = Paths.get(JSON_FILE);
+		FileWriter writer = new FileWriter(path.toFile());
+		for (Map.Entry<String, AddressBook> entry : cityBookMap.entrySet()) {
+			entry.getValue().getContactList().forEach(contact -> {
+				String json = gson.toJson(contact);
+				try {
+					writer.write(json);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+		writer.close();
+	}
+
+	/**
+	 * Usecase15 using GSON reading from a JSON file
+	 */
+	public void readDataFromJSON() {
+		Gson gson = new Gson();
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(Paths.get(JSON_FILE).toFile()));
+			JsonStreamParser parser = new JsonStreamParser(bufferedReader);
+			while (parser.hasNext()) {
+				JsonElement jsonElement = parser.next();
+				if (jsonElement.isJsonObject()) {
+					Contact contact = gson.fromJson(jsonElement, Contact.class);
+					System.out.println(contact);
+				}
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
 		}
 	}
 }
